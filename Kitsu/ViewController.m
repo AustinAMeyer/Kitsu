b977c38c134d1bd8a3105346d6cbe1af071fb9c2
Austin Meyer <Austin.a.Meyer@gmail.com>
2019-08-06 22:20:42 -0500

    Connected to API and added titles and images to table

--- Kitsu/ViewController.m
+++ Kitsu/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource>
-
+@property (strong, nonatomic) NSArray *canonicalTitle;
+@property (strong, nonatomic) NSArray *posterImage;
 @end
 
 @implementation ViewController
@@ -17,7 +18,21 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSError *error;
+    NSString *kitsuUrl = [NSString stringWithFormat: @"https://kitsu.io/api/edge/anime?page[limit]=20&page[offset]=0"];
+    NSData *kitsuData = [NSData dataWithContentsOfURL: [NSURL URLWithString:kitsuUrl]];
+    NSDictionary *kitsuJson = [NSJSONSerialization JSONObjectWithData:kitsuData options:kNilOptions error:&error];
+    //NSLog(@"json: %@", kitsuJson);
+    
+    self.canonicalTitle = [kitsuJson valueForKeyPath:@"data.attributes.canonicalTitle"];
+    self.posterImage = [kitsuJson valueForKeyPath:@"data.attributes.posterImage.tiny"];
+    
+    //NSLog(@"The content of arry is%@",self.canonicalTitle);
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 100;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -27,17 +42,25 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 5;
+    return 20;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     
-    cell.textLabel.text = [NSString stringWithFormat:@"Row : %ld", indexPath.row + 1];
     
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", self.canonicalTitle[indexPath.item]];
+    
+    NSString *imgString = self.posterImage[indexPath.item];
+    
+    NSData *imageData = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:imgString]];
+    
+    cell.imageView.image = [UIImage imageWithData: imageData];
+
     return cell;
 }
+
 
 
 @end
